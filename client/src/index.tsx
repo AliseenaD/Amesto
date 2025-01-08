ed33b0ba9945b37c7f5b8afdef5705cef45e2351
aliseena1 <aliseena@live.unc.edu>
2025-01-08 18:45:33 -0500

    Navbar changes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthTokenProvider } from './AuthTokenContext';
 import { CartProvider } from './CartContext.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/pages/Home.tsx';
 import Phones from './components/pages/Phones.tsx';
 import Speakers from './components/pages/Speakers.tsx';
@@ -21,25 +21,38 @@ import NavBar from './components/elements/NavBar.tsx';
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
+// Conditionally render the navbar
+const Layout = () => {
+  const location = useLocation();
+  const showNavBar = location.pathname !== '/register';
+
+  // Conditionally render navbar depending on the route
+  return (
+    <>
+      {showNavBar && <NavBar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/phones' element={<Phones />} />
+        <Route path='/speakers' element={<Speakers />} />
+        <Route path='/news' element={<News />} />
+        <Route path='/headphones' element={<Headphones />} />
+        <Route path='/accessories' element={<Accessories />} />
+        <Route path='watches' element={<Watches />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </>
+  );
+}
+
 root.render(
   <React.StrictMode>
       <BrowserRouter>
           <AuthTokenProvider>
             <CartProvider>
               <ToastContainer position='top-center' />
-                <NavBar />
-                <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/phones' element={<Phones />} />
-                  <Route path='/speakers' element={<Speakers />} />
-                  <Route path='/news' element={<News />} />
-                  <Route path='/headphones' element={<Headphones />} />
-                  <Route path='/accessories' element={<Accessories />} />
-                  <Route path='watches' element={<Watches />} />
-                  <Route path='/cart' element={<Cart />} />
-                  <Route path='/profile' element={<Profile />} />
-                  <Route path='/register' element={<Register />} />
-                </Routes>
+                <Layout />
             </CartProvider>
           </AuthTokenProvider>
       </BrowserRouter>
